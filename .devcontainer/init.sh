49b0c10d79b37c3699298f8fac78541c22701865
winnet101 <115325533+winnet101@users.noreply.github.com>
2025-10-28 00:27:37 +0000

    Added comments

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 set -e
+# break instantly on errors
 
 # Move to /opt
 cd /opt
 
 # Get the latest release tag from GitHub API
 LATEST_URL=$(curl -s https://api.github.com/repos/wpilibsuite/vscode-wpilib/releases/latest \
-  | grep "browser_download_url" \
+  | grep "browser_download_url" \ 
   | grep ".vsix" \
   | cut -d '"' -f 4)
+  # -d: delimit by char "
+  # -f: fields
 
 # Extract filename
 FILENAME=$(basename "$LATEST_URL")
 
 echo "Downloading latest WPILib VS Code extension: $FILENAME"
 wget -q "$LATEST_URL" -O "$FILENAME"
+# -q quiet mode
+# -O download to file named "$FILENAME"
 
-# Optional: install globally for VS Code in Codespaces
 code --install-extension "/$FILENAME" --force
